929d37923a4cb4444e31ade29efb4b502948b2a4
Shiqi Jiang <jiangshiqi@autohome.com.cn>
2018-07-16 11:32:12 +0800

    Initial commit

---
+++ FitCook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FitCook
+//
+//  Created by Jay on 2018/7/16.
+//  Copyright © 2018年 Joyoki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FitCook/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FitCook
+//
+//  Created by Jay on 2018/7/16.
+//  Copyright © 2018年 Joyoki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
