34ee6eb0edb2904c6d797fc24c79fb6aa6904140
Shiqi Jiang <jiangshiqi@autohome.com.cn>
2018-08-07 18:40:12 +0800

    some code

---
+++ FitCook/Common/Realm/FCRealmManager.h
@@ -0,0 +1,13 @@
+//
+//  FCRealmManager.h
+//  FitCook
+//
+//  Created by Jay on 2018/8/7.
+//  Copyright © 2018年 Joyoki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FCRealmManager : NSObject
+
+@end
---
+++ FitCook/Common/Realm/FCRealmManager.m
@@ -0,0 +1,14 @@
+//
+//  FCRealmManager.m
+//  FitCook
+//
+//  Created by Jay on 2018/8/7.
+//  Copyright © 2018年 Joyoki. All rights reserved.
+//
+
+#import "FCRealmManager.h"
+#import <Realm/Realm.h>
+
+@implementation FCRealmManager
+
+@end
